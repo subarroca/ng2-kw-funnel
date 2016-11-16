b9648a9e0fa4e5a64cf1e3186936183ec90c9aa8
Salvador Subarroca <subarroca@gmail.com>
2016-11-16 23:19:19 +0100

    refactor(funnel-segment): create subcomponent for demo sake

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@
 
 
 // ANGULAR
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 
 
@@ -32,7 +32,7 @@ import {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   colors = {
     indigo: '#14143e',
     pink: '#fd1c49',
@@ -44,25 +44,50 @@ export class AppComponent {
     white: '#fff'
   };
 
+  mode: string = 'lin'; // 'log'
+
+  testMultiplier = 10;
+
+  data1: number[] = [100, 65, 50, 10];
+  data2: number[] = [100, 45, 40, 35];
 
   segments: FunnelSegment[] = [
     new FunnelSegment({
-      value: 100,
+      value: 100 * this.testMultiplier,
       color: this.colors.purple
     }),
     new FunnelSegment({
-      value: 65,
+      value: 65 * this.testMultiplier,
       color: this.colors.pink
     }),
     new FunnelSegment({
-      value: 50,
+      value: 50 * this.testMultiplier,
       color: this.colors.orange
     }),
     new FunnelSegment({
-      value: 10,
+      value: 10 * this.testMultiplier,
       color: this.colors.yellow,
       labelColor: this.colors.indigo
     })
   ];
+
+  selectedData: number;
+
+
+  constructor() { }
+
+  ngOnInit() {
+    this.setData1();
+  }
+
+  setData1() {
+    this.selectedData = 1;
+    this.segments.forEach((segment, i) => segment.value = this.data1[i]);
+  }
+
+  setData2() {
+    this.selectedData = 2;
+    this.segments.forEach((segment, i) => segment.value = this.data2[i]);
+  }
 
 }
--- src/app/funnel/funnel-segment/funnel-segment.component.ts
+++ src/app/funnel/funnel-segment/funnel-segment.component.ts
@@ -1,15 +1,238 @@
-import { Component, OnInit } from '@angular/core';
+/**
+ * @category   Component
+ * @package    com.kiwity.ng2-kw-funnel.funnel
+ * @author     Salvador Subarroca (subarroca@gmail.com)
+**/
+
+
+
+
+// ANGULAR
+import { Component, OnInit, Input, ViewChild, ElementRef, OnChanges } from '@angular/core';
+
+
+// EXTERNAL
+import { Observable } from 'rxjs/Rx';
+
+
+
+// OWN
+import {
+  Segment,
+  Point
+} from '../shared';
+
+
 
 @Component({
-  selector: 'ng2-kw-funnel-segment',
+  selector: 'g[ng2-kw-funnel-segment]',
   templateUrl: './funnel-segment.component.html',
   styleUrls: ['./funnel-segment.component.scss']
 })
-export class FunnelSegmentComponent implements OnInit {
+export class FunnelSegmentComponent implements OnInit, OnChanges {
+  @Input() from: number;
+  @Input() to: number;
+  @Input() color: string;
+  @Input() labelColor: string;
+  @Input() max: number;
+  @Input() min: number;
+  @Input() width: number;
+  @Input() height: number;
+  @Input() graphMode: string;
+
+  @ViewChild('animate') segmentAnimate: ElementRef;
+
+
+  slope: number = .2;
+  RADIUS: number = 10 / 100;
+
+  previousPath: string;
+  currentPath: string;
+  _currentPath: string;
+
+
+
+
 
   constructor() { }
 
   ngOnInit() {
+    this._currentPath = this.drawInitialSegment();
+    this.calculateSegments();
+  }
+
+  ngOnChanges(changes) {
+    if (changes.graphMode || changes.to || changes.from) {
+      this.calculateSegments();
+    }
+  }
+
+
+  getLinHeight(): Segment {
+    let max = this.max * (1 + this.slope);
+    let delta = this.height / max;
+
+    return new Segment({
+      start: new Point({
+        x: 0,
+        y: ((!this.from) ? this.height : this.from * delta) || 0
+      }),
+      end: new Point({
+        x: 0,
+        y: this.to * delta || 0
+      })
+    });
+  }
+
+  // THIS IS NOT WORKING
+  getLogHeight(): Segment {
+    // let max = this.max * (1 + this.slope);
+    let logDelta = this.max / this.min;
+    // let delta = this.height * Math.log(1 / this.height);
+
+    return new Segment({
+      start: new Point({
+        x: 0,
+        y: ((!this.from) ? this.height : Math.log(logDelta * this.from / this.max) * this.height) || 0
+      }),
+      end: new Point({
+        x: 0,
+        y: Math.log(logDelta * this.to / this.max) * this.height || 0
+      })
+    });
+  }
+
+
+
+
+
+
+
+
+  calculateSegments() {
+    this.getPath();
+
+    // animate morphing but let some time to enable morphing in every case
+    Observable.timer(0)
+      .first()
+      .subscribe(timer => {
+        this.updateSegments();
+      });
+  }
+
+  updateSegments() {
+    this.currentPath = this._currentPath;
+
+    this.segmentAnimate.nativeElement.beginElement();
+  }
+
+  getPath() {
+    this.previousPath = this._currentPath;
+    this._currentPath = this.getSegment();
+  }
+
+
+
+
+
+
+
+  // SEGMENT
+  protected drawSegment(segment: Segment): string {
+    // from axe right
+    // to previous right
+    // to end this.slope
+    // to current left
+
+    // to -current left
+    // to -end this.slope
+    // to -previous right
+
+    return `
+      M0, ${this.height / 2}
+      ${Point.getRoundedCorner(
+        {
+          x: 0,
+          y: this.height / 2 - segment.start.y / 2
+        }, {
+          x: this.width * this.slope,
+          y: this.height / 2 - segment.end.y / 2
+        }, {
+          x: this.width,
+          y: this.height / 2 - segment.end.y / 2
+        },
+        this.width * this.RADIUS)}
+      
+
+      ${Point.getRoundedCorner(
+        {
+          x: this.width,
+          y: this.height / 2 + segment.end.y / 2
+        }, {
+          x: this.width * this.slope,
+          y: this.height / 2 + segment.end.y / 2
+        }, {
+          x: 0,
+          y: this.height / 2 + segment.start.y / 2
+        },
+        this.width * this.RADIUS)}
+      Z`;
+  }
+
+  protected drawInitialSegment(): string {
+    // from axe right
+    // to previous right
+    // to end this.slope
+    // to current left
+
+    // to -current left
+    // to -end this.slope
+    // to -previous right
+
+    return `
+      M0, ${this.height / 2}
+      ${Point.getRoundedCorner(
+        {
+          x: 0,
+          y: this.height / 2
+        }, {
+          x: this.width * this.slope,
+          y: this.height / 2
+        }, {
+          x: this.width,
+          y: this.height / 2
+        },
+        this.width * this.RADIUS)}
+      
+
+      ${Point.getRoundedCorner(
+        {
+          x: this.width,
+          y: this.height / 2
+        }, {
+          x: this.width * this.slope,
+          y: this.height / 2
+        }, {
+          x: 0,
+          y: this.height / 2
+        },
+        this.width * this.RADIUS)}
+      Z`;
+  }
+
+  protected getSegment(): string {
+    let segment: Segment;
+
+    switch (this.graphMode) {
+      case 'lin':
+        segment = this.getLinHeight();
+        break;
+
+      case 'log':
+        segment = this.getLogHeight();
+        break;
+    }
+    return this.drawSegment(segment);
   }
 
 }
